38af0ea1c4b0778d81a9a57e9bfb720de805198b
oleksii.b <oleksii.b@dev.tagsoft.co>
2024-08-13 09:58:39 +0300

    completed 1-5 practical tasks

---
+++ practice-1/solana-training/check-balance.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import {
+    Connection,
+    LAMPORTS_PER_SOL,
+    PublicKey,
+    clusterApiUrl,
+} from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(`⚡️ Connected to devnet`);
+
+const publicKey = new PublicKey(process.env["PUBLIC_KEY"]);
+
+// @ts-ignore
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+    `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+);
+
+//npx esrun check-balance.ts
